b8cedc91c685711fd74b463388e89fed41e75b12
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-09-15 18:20:36 -0400

    adding new projects

---
+++ SunFadeInAndOut/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $(".the-sun").fadeOut(700)
+    });
+    $(".btn2").click(function(){
+        $(".the-sun").fadeIn(1000);
+    });
+});
