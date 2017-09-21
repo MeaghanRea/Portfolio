0bf0825862cd5a18a41ade5514f199a7d33e9644
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-09-21 17:48:17 -0400

    adding new project AchyBreakyHeart

---
+++ AchyBreakyHeart/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$('#heart1').mouseenter(function() {
+		alert('Don\'t tell my heart...');
+	});
+	$('#heart2').mouseenter(function() {
+		alert('My achy, breaky heart...');
+	});
+	$('#heart3').mouseenter(function() {
+		alert('I just don\'t think he\'d understand...');
+	});
+	$('#heart4').mouseenter(function() {
+		alert('And if you tell my heart, my achy, breaky heart...');
+	});
+	$('#heart5').mouseenter(function() {
+		$(this).effect('explode');
+		alert('He might blow up and kill this man. (wooooo-oo-oooooo!)');
+	});
+	$('#heart6').mouseenter(function() {
+		alert('No hearts were achy, breaky\'d during the making of this exercise.');
+	});
+	$('#heart7').mouseenter(function() {
+		alert('Written by Donald L. Von Tress, the song was made popular in 1992 by Billy Ray Cyrus.');
+	});
+});
+
+
+
+
+
+
