329ac6598da3d85c49dc591246f69ed15cfd3b4e
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-10-02 15:06:51 -0400

    got the fadeOut and fadeIn to work!

--- GeometricShapes/script.js
+++ GeometricShapes/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 	$('.triangle-down').mouseenter(function() {
-		alert('Don\'t tell my heart...');
+		$(this).fadeOut (2000);
+		$(this).fadeIn (2000);
+	});
+	$('.triangle-up').mouseenter(function() {
+		$(this).fadeOut (1000);
+		$(this).fadeIn (1000);
+	});
+	$('.triangle2-up').mouseenter(function() {
+		$(this).fadeOut (500);
+		$(this).fadeIn (500);
+	});
+	$('.triangle2-down').mouseenter(function() {
+		$(this).fadeOut (1500);
+		$(this).fadeIn (1500);
 	});
 });
 
