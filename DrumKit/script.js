b471ebdd881ecddb77206fcd902ae96cdfc82d53
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-10-04 23:05:22 -0400

    edits to js file

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -2,18 +2,16 @@ function removeTransition(e) {
 	if (e.propertyName !== 'transform') return; //skip if it's not a transform
 	this.classList.remove('playing');
 }
-
 function playSound(e) {
 	const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
 	const key = document.querySelector('div[data-key="${e.keyCode}"]');
 	if (!audio) return; // stop the function from running alltogether
 	
+	key.classList.add('playing');
 	audio.currentTime = 0; // rewind to the start
 	audio.play();
-	key.classList.add('playing');
 }
 
-const keys = document.querySelectorAll('.key');
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
 window.addEventListener('keydown', playSound);
