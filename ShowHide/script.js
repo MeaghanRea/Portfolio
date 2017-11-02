16b74fa7efedaf5e826e777c175545087b23f2c6
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-11-01 22:28:03 -0400

    adding new project to site

---
+++ ShowHide/script.js
@@ -0,0 +1,26 @@
+// $(document).ready(() => {
+// 	$('.btn-hide').on('click', () => {
+// 		$('.astronaut').hide();
+// 	});
+// });
+
+$(document).ready(() => {
+  $('.btn-hide').on('click', () => {
+    $('.astronaut').hide();
+});
+
+
+// $(document).ready(() => {
+//   $('.hide-button').on('click', () => {
+//     $('.first-image').hide();
+//   });
+  
+//   $('.show-button').on('click', () => {
+//     $('.first-image').show();
+//   });
+  
+//   $('.toggle-button').on('click', () => {
+//     $('.first-image').toggle();
+//   });
+  
+});
