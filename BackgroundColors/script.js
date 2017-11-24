eec6d4df06829c604e38341ced6639444db911dc
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-11-24 11:30:47 -0500

    removed unneccessary facebook animation from JavaScript file

--- BackgroundColors/script.js
+++ BackgroundColors/script.js
@@ -26,13 +26,3 @@ $(document).ready(function(){
 		$(this).css("color", "black");
 	});
 });
-
-
-/* facebook share link */
-(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
