ac7511ea56abfb73fab704d65a0abad1f1f2d846
RainbowMoonbeams <meaghan.rea@gmail.com>
2017-11-24 20:07:10 -0500

    experiementing with jquery

--- BlocksAndOptions/script.js
+++ BlocksAndOptions/script.js
@@ -1,24 +1,26 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function changeColor() {
-	document.getElementsByClassName()
-}
+$(document).ready(function() {
+	$('#color-blue').click(function() {
+		$(this).css($('.color-block'), "blue");
+	})
+})
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// window.onclick = function(event) {
+// 	if (!event.target.matches('.btn-color-picker')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// 		var dropdowns = document.getElementsByClassName("dropdown-content");
+// 		var i;
+// 		for (i = 0; i < dropdowns.length; i++) {
+// 			var openDropdown = dropdowns[i];
+// 			if (openDropdown.classList.contains('show')) {
+// 				openDropdown.classList.remove('show');
+// 			}
+// 		}
+// 	}
+// }
